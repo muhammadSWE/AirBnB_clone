e025622b14445dbaf17a0f8e4f704ce54472a0c6
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-03-07 12:19:40 +0200

    Initialize base_model & file_storage

---
+++ models/base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+This module contains the code for the BaseModel class.
+
+Classes:
+    BaseModel: the base model class
+'''
+import uuid
+from datetime import datetime
+from models import storage
+
+
+class BaseModel:
+    pass
---
+++ models/engine/file_storage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+This module contains the code for the FileStorage class.
+
+Classes:
+    FileStorage: the file storage class
+'''
+import json
+
+
+class FileStorage:
+    '''
+    This class serializes instances to a JSON file and
+    deserializes JSON file to instances.
+    '''
+    pass
