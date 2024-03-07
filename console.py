2591470901ee4a2effd357ac8645d79f11211d19
Daniel Kamel <34145238+daniel-kamel@users.noreply.github.com>
2024-03-07 13:29:55 +0200

    Add 'create', 'show', 'all' & 'destroy' commands to the console

--- console.py
+++ console.py
@@ -2,8 +2,10 @@
 '''
 This module contains the code for the hbnb Console.
 '''
-
+from models.base_model import BaseModel
+from models import storage
 import cmd
+
 
 class HBNBCommand(cmd.Cmd):
     '''
@@ -11,6 +13,8 @@ class HBNBCommand(cmd.Cmd):
     '''
 
     prompt = '(hbnb) '
+    # Available classes
+    classes = {'BaseModel': BaseModel}
 
     def do_quit(self, arg):
         '''
@@ -37,12 +41,115 @@ class HBNBCommand(cmd.Cmd):
         '''
         print("Quit command to exit the program.")
         print("Usage: EOF")
-        
+
+    def do_create(self, arg):
+        '''
+        Create a class instance.
+        '''
+        if not arg:
+            print("** class name missing **")
+            return
+        elif arg not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        new_instance = HBNBCommand.classes[arg]()
+        new_instance.save()
+        print(new_instance.id)
+
+    def help_create(self):
+        '''
+        Help information for the create command.
+        '''
+        print("Create a class instance.")
+        print("Usage: create <className>")
+
+    def do_show(self, arg):
+        '''
+        Method to show an instance.
+        '''
+        if not arg:
+            print("** class name missing **")
+            return
+        args = arg.split()
+        if args[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = args[0] + "." + args[1]
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+        print(storage.all()[key])
+
+    def help_show(self):
+        '''
+        Help information for the show command.
+        '''
+        print("Show an instance.")
+        print("Usage: show <className> <objectId>")
+
+    def do_delete(self, arg):
+        '''
+        Delete an instance of a class.
+        '''
+        if not arg:
+            print("** class name missing **")
+            return
+        args = arg.split()
+        if args[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = args[0] + "." + args[1]
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+        del storage.all()[key]
+        storage.save()
+
+    def help_delete(self):
+        '''
+        Help information for the delete command.
+        '''
+        print("Delete an instance of a class.")
+        print("Usage: delete <className> <objectId>")
+
+    def do_all(self, arg):
+        '''
+        Print all instances of a class.
+        '''
+        if not arg:
+            print("** class name missing **")
+            return
+        if arg not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        objects = storage.all()
+        for key in objects:
+            if key.split(".")[0] == arg:
+                print(objects[key])
+
+    def help_all(self):
+        '''
+        Help information for the all command.
+        '''
+        print("Prints all string representation of all instances.\n"
+              "Usage: all\n"
+              "\tPrints the string representation of all instances of all classes\n"
+              "Usage: all <className>\n"
+              "\tPrints the string representation of all instances of <className>\n"
+              )
+
     def emptyline(self):
         '''
         Handles empty lines.
         '''
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
