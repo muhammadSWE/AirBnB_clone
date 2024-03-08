264893d9be23db4fa5b0991addff713980cbf3ae
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2024-03-08 22:13:04 +0200

    updated FileStorage with the rest of the classes from tasks 8 and 9

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,6 +9,12 @@ import json
 import datetime
 import os
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
@@ -44,7 +50,14 @@ class FileStorage:
         '''
         Returns a dictionary of valid classes and their references
         '''
-        classes = {"BaseModel": BaseModel}
+        
+        classes = {"BaseModel": BaseModel,
+                    "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
         return classes
 
     def reload(self):
@@ -67,6 +80,34 @@ class FileStorage:
             "BaseModel":
                      {"id": str,
                       "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime}
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
                       }
         return attributes
