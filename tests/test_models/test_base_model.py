0c803df275b45329f21db2d39e7264ce644a0cd4
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2024-03-09 22:07:19 +0200

    refactored testbasemodel to pass the pycodestyle checks

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,13 +1,14 @@
 import unittest
 from models.base_model import BaseModel
-from models import storage 
+from models import storage
 import datetime
+
 
 class TestBaseModel(unittest.TestCase):
     """ Unit tests for the BaseModel class """
 
     def test_init(self):
-        # Test normal instantiation 
+        # Test normal instantiation
         my_model = BaseModel()
         self.assertIsNotNone(my_model.id)
         self.assertIsNotNone(my_model.created_at)
@@ -15,10 +16,10 @@ class TestBaseModel(unittest.TestCase):
 
         # Test instantiation with kwargs
         my_model2 = BaseModel(name="MyModel", age=89)
-        self.assertEqual(my_model2.name, "MyModel") 
+        self.assertEqual(my_model2.name, "MyModel")
         self.assertEqual(my_model2.age, 89)
 
-    def test_str(self): 
+    def test_str(self):
         model = BaseModel()
         expected_str = f"[BaseModel] ({model.id}) {model.__dict__}"
         self.assertEqual(str(model), expected_str)
@@ -33,8 +34,11 @@ class TestBaseModel(unittest.TestCase):
         model = BaseModel()
         model_dict = model.to_dict()
         self.assertEqual(model_dict["__class__"], "BaseModel")
-        self.assertEqual(model_dict["created_at"], model.created_at.isoformat())
-        self.assertEqual(model_dict["updated_at"], model.updated_at.isoformat())
+        self.assertEqual(model_dict["created_at"],
+                         model.created_at.isoformat())
+        self.assertEqual(model_dict["updated_at"],
+                         model.updated_at.isoformat())
+
 
 if __name__ == "__main__":
     unittest.main()
