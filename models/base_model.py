072b649558aa0cb866c1948fd4aec579adac0c5e
Muhammad Hussein <muhammad.a.mahmoud@outlook.com>
2024-03-09 22:01:19 +0200

    refactored basemodel to pass pycodestyle checks

--- models/base_model.py
+++ models/base_model.py
@@ -19,14 +19,11 @@ class BaseModel:
         - *args: list of arguments
         - **kwargs: dictionary of key-values arguments
         '''
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[key] = kwargs[key]
+        if kwargs is not None and kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -37,8 +34,11 @@ class BaseModel:
         '''
         Returns a string representation of an instance
         '''
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            type(self).__name__,
+            self.id,
+            self.__dict__
+        )
 
     def save(self):
         '''
